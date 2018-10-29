68933e3f2f5c6a3cab43b3f4960bd4f774dbda52
brobeson <brobeson@users.noreply.github.com>
2018-10-28 23:14:25 -0600

    improve execution of lint tools
    I changed the lint.sh script, to automatically run lint tools on all
    Python files.

--- lint.sh
+++ lint.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-set -v
+#set -v
 
 # Lizard doesn't properly scan Python files without the extension. So, create a copy
 # with the extension, and scan that.
-cp cmake-tidy/cmake-tidy cmake-tidy/cmake-tidy.py && \
-    lizard --CCN=10 --length=20 --arguments=6 --warnings_only --modified cmake-tidy/cmake-tidy.py && \
-    rm cmake-tidy/cmake-tidy.py
+cp cmake-tidy/cmake-tidy cmake-tidy/cmake-tidy.py
+find . -name *.py \
+    -exec lizard --CCN=10 --length=20 --arguments=6 --warnings_only --modified {} \;
+rm cmake-tidy/cmake-tidy.py
