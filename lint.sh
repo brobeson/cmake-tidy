8ed1db4f562bdca5121128f0b9e3dac2aae8274f
brobeson <brobeson@users.noreply.github.com>
2019-06-01 12:39:06 -0600

    run Lizard on Travis-CI
    - I added a CI dependency to install Lizard.
    - I added a command to run Lizard on Travis-CI.
    - I removed lint.sh; it's not necessary anymore.

--- lint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -v
-
-# Lizard doesn't properly scan Python files without the extension. So, create a copy
-# with the extension, and scan that.
-cp cmake-tidy/cmake-tidy cmake-tidy/cmake-tidy.py && \
-    lizard --CCN=10 --length=20 --arguments=6 --warnings_only --modified cmake-tidy/cmake-tidy.py && \
-    rm cmake-tidy/cmake-tidy.py
