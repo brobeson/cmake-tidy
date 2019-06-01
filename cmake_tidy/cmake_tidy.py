d9db86f34c3f898a236fb78cf5af0d35a9fb9508
brobeson <brobeson@users.noreply.github.com>
2019-06-01 11:25:50 -0600

    reformat according to black
    I ran black and committed the formatting changes.

--- cmake_tidy/cmake_tidy.py
+++ cmake_tidy/cmake_tidy.py
@@ -1,27 +1,34 @@
 import argparse
 import sys
 
-__version__ = '0.0.0'
+__version__ = "0.0.0"
+
 
 def make_parser():
     parser = argparse.ArgumentParser(
-        description='Analyze CMake scripts for formatting and style mistakes.',)
-    parser.add_argument('--version',
-        action='version',
+        description="Analyze CMake scripts for formatting and style mistakes."
+    )
+    parser.add_argument(
+        "--version",
+        action="version",
         version=__version__,
-        help='Display the version, and exit.')
-    parser.add_argument('files',
-        default='./CMakeLists.txt',
-        nargs='*',
-        help='The CMake files to analyze. The default is ./CMakeLists.txt')
+        help="Display the version, and exit.",
+    )
+    parser.add_argument(
+        "files",
+        default="./CMakeLists.txt",
+        nargs="*",
+        help="The CMake files to analyze. The default is ./CMakeLists.txt",
+    )
     return parser
+
 
 def main():
     print("Running cmake_lint.main()")
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = make_parser()
     arguments = parser.parse_args()
     sys.exit()
